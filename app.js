b9bbde4d1502a350eef9753bdac4aa20caddf346
Curtis Patterson <Dpatter94@yahoo.com>
2024-04-15 14:35:31 -0400

    Commit 1

---
+++ app.js
@@ -0,0 +1,15 @@
+// create variables that will output the correct comibnation.
+
+const combo1 = 5 * 2;
+const combo2 = 80 / 2;
+const combo3 = 40 - 1;
+
+// Create a popup window explaining you have gained access to the vault.
+
+alert(
+  "You have received this message because you have been chosen to open an important vault."
+);
+// Another popup letting you know what the combination is and it outputs the variables and list them out.
+alert(
+  "Here is the secret combination:" + " " + combo1 + " " + combo2 + " " + combo3
+);
